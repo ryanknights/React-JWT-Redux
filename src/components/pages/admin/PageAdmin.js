2c9bd6035de78d91f0e994177d7309fe9dc03bcf
Ryan Knights <ryanknights@Ryan-Knightss-iMac.local>
2017-11-08 10:02:32 +0000

    Added admin user list

--- src/components/pages/admin/PageAdmin.js
+++ src/components/pages/admin/PageAdmin.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'; 
+import { getUsers, removeUser } from '../../../actions/users';
+import { setFeedback } from '../../../actions/feedback';
+
+import UsersList from './UsersList';
 
 class PageAdmin extends Component {
+	componentDidMount() {
+		if (!this.props.fetching) {
+			this.props.getUsers()
+				.catch(error => {
+					this.props.setFeedback({message: 'There was a problem retrieving the users', 'type': 'warning'});
+				});
+		}
+	}
 	render () {
 		return (
 			<div>
 				<h1 className="display-4">Admin</h1>
+				<UsersList 
+					users={this.props.users}
+					fetching={this.props.fetching}
+					setFeedback={this.props.setFeedback}
+					remove={this.props.removeUser}
+				/>
 			</div>
 		);
 	}
@@ -13,8 +32,18 @@ class PageAdmin extends Component {
 
 function mapStateToProps(state) {
 	return {
-		auth: state.auth
+		auth: state.auth,
+		users: state.users.all,
+		fetching: state.users.isFetching
 	}
 }
 
-export default connect(mapStateToProps)(PageAdmin);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+	getUsers: getUsers,
+	removeUser: removeUser,
+	setFeedback: setFeedback,
+  }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PageAdmin);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,14 @@ import feedback from './feedback';
 import loader from './loader';
 import auth from './auth';
 import posts from './posts';
+import users from './users';
 
 const rootReducer = combineReducers({
 	feedback,
 	loader,
 	auth,
 	posts,
+	users,
 	router: routerReducer
 });
 
